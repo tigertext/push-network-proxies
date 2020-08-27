64fcbc2933e415ac3013ee21aa9e8cdcd702ab5c
Kevin Dinicola <kdinicola@tigerconnect.com>
2020-08-27 11:45:14 -0700

    Added modifications to allow ease of use for mocking pushes from erl_services

--- apns-java/impl/src/main/java/org/jboss/aerogear/proxy/command/ApnsProxyCommand.java
+++ apns-java/impl/src/main/java/org/jboss/aerogear/proxy/command/ApnsProxyCommand.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.cert.X509Certificate;
+import java.security.interfaces.ECPublicKey;
 import java.util.Random;
 
 import com.relayrides.pushy.apns.MockApnsServer;
@@ -33,7 +35,7 @@ public class ApnsProxyCommand extends NotificationRegisterEndpoint {
     @Option(name = "--apnsMockGatewayHost", description = "defaults to 127.0.0.1")
     private String apnsMockGatewayHost = "127.0.0.1";
 
-    @Option(name = "--apnsMockGatewayPort", description = "defaults to 16002")
+    @Option(name = "--apnsMockGatewayPort", description = "defaults to 18443")
     private int apnsMockGatewayPort = 18443;
 
     // Certificate related
@@ -50,7 +52,12 @@ public class ApnsProxyCommand extends NotificationRegisterEndpoint {
     @Option(name = "--apnsKeystoreAlgorithm", description = "defaults to sunx509")
     private String apnsKeystoreAlgorithm = "sunx509";
 
+    @Option(name = "--deviceTokens", description = "must specify", required = true)
+    private String deviceTokens = "";
+
     private String resourceServerStore = "serverStore.p12";
+
+    private String clientSslCert = "apn_sandbox_consumer.pem";
 
     @Override
     public void run() {
@@ -60,12 +67,17 @@ public class ApnsProxyCommand extends NotificationRegisterEndpoint {
                     getInputStream(), apnsKeystorePassword);
 
             final MockApnsServerBuilder serverBuilder = new MockApnsServerBuilder()
+                    .setTrustedClientCertificateChain(getClientSslCert())
                     .setServerCredentials(new X509Certificate[] { (X509Certificate) privateKeyEntry.getCertificate() }, privateKeyEntry.getPrivateKey(), null)
                     .setEventLoopGroup(new NioEventLoopGroup(4));
 
+
             final MockApnsServer server = serverBuilder.build();
 
-            server.registerDeviceTokenForTopic("org.aerogear.test", generateRandomToken(), null);
+            String[] tokens = deviceTokens.split(",");
+            for (String token : tokens) {
+                server.registerDeviceTokenForTopic("com.tigertext.tigertext", token, null);
+            }
 
             server.start(apnsMockGatewayPort).await();
 
@@ -94,16 +106,13 @@ public class ApnsProxyCommand extends NotificationRegisterEndpoint {
         }
     }
 
-    private String generateRandomToken() {
-        final byte[] tokenBytes = new byte[TOKEN_LENGTH];
-        new Random().nextBytes(tokenBytes);
-
-        final StringBuilder builder = new StringBuilder(TOKEN_LENGTH * 2);
-
-        for (final byte b : tokenBytes) {
-            builder.append(String.format("%02x", b));
+    private InputStream getClientSslCert() {
+        InputStream stream;
+        stream = SSLHelper.class.getResourceAsStream("/" + clientSslCert);
+        if (stream == null) {
+            throw new RuntimeException("Unable load client ssl cert");
         }
-
-        return builder.toString();
+        return stream;
     }
+
 }
